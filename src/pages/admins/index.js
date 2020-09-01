20b8e5218e438e6994c3797b5fc859fe6b904638
tcap_Abra <Guerrero-rollon.A1@trans-cosmos.co.jp>
2020-09-01 13:06:01 +0800

    test

--- src/pages/admins/index.js
+++ src/pages/admins/index.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Route, Switch } from "react-router-dom";
 import "../pages.scss";
 import { getUsers } from "../../graphqlAPI";
+import AdminForm from "./admin-form";
 
 class Admins extends PureComponent {
   constructor(props) {
@@ -30,50 +31,57 @@ class Admins extends PureComponent {
   render() {
     const { tableUser } = this.state;
     return (
-      <div className="super-container">
-        <h2>Admins</h2>
-        <NavLink to="/superadmin/superform">
-          <button type="button" className="btn btn-info">
-            Add
-          </button>
-        </NavLink>
-        <div className="tableData">
-          <h1 id="title">Table Data</h1>
-          <table id="usersdata">
-            <thead>
-              <tr>
-                <th>Email</th>
-                <th>Full Name</th>
-                <th>Role</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tableUser.length > 0 ? (
-                tableUser.map((user, index) => {
-                  return (
-                    <tr key={index}>
-                      <td>{user.email}</td>
-                      <td>
-                        {user.first_name} {user.last_name}
-                      </td>
-                      <td>{user.role}</td>
-                      <td className="btn-container">
-                        <button className="edit">edit</button>
-                        <button className="delete">delete</button>
-                      </td>
+      <Switch>
+        <Route path="/admins/add">
+          <AdminForm />
+        </Route>
+        <Route>
+          <div className="super-container">
+            <h2>Admins</h2>
+            <NavLink to="/admins/add">
+              <button type="button" className="btn btn-info">
+                Add
+              </button>
+            </NavLink>
+            <div className="tableData">
+              <h1 id="title">Table Data</h1>
+              <table id="usersdata">
+                <thead>
+                  <tr>
+                    <th>Email</th>
+                    <th>Full Name</th>
+                    <th>Role</th>
+                    <th>Actions</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {tableUser.length > 0 ? (
+                    tableUser.map((user, index) => {
+                      return (
+                        <tr key={index}>
+                          <td>{user.email}</td>
+                          <td>
+                            {user.first_name} {user.last_name}
+                          </td>
+                          <td>{user.role}</td>
+                          <td className="btn-container">
+                            <button className="edit">edit</button>
+                            <button className="delete">delete</button>
+                          </td>
+                        </tr>
+                      );
+                    })
+                  ) : (
+                    <tr>
+                      <td colSpan="5">Loading...</td>
                     </tr>
-                  );
-                })
-              ) : (
-                <tr>
-                  <td colSpan="5">Loading...</td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        </div>
-      </div>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </Route>
+      </Switch>
     );
   }
 }
