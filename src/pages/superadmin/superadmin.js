590d1d4fb6149fdba5ef56ab5c7e50d53f30452b
James Dollente <psalmybald@yahoo.com>
2020-09-01 12:34:44 +0800

    add table

--- src/pages/superadmin/superadmin.js
+++ src/pages/superadmin/superadmin.js
@@ -3,9 +3,33 @@ import { Route, NavLink } from "react-router-dom";
 import SuperForm from "./superform";
 import SuperTable from './table';
 import "../pages.scss";
+import { getUsers } from "../../graphqlAPI";
 
 class SuperAdmin extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableUser: [
+        {
+          email: "",
+          first_name: "",
+          last_name: "",
+          role: "",
+          created_at: "",
+          updated_at: "",
+        },
+      ],
+    };
+  }
+  async componentDidMount() {
+    const { users } = this.props;
+    await getUsers(users).then((data) => {
+      this.setState({ tableUser: data.data.users });
+    });
+  }
+
   render() {
+    const { tableUser } = this.state;
     return (
       <div className="super-container">
         <p>SuperAdmin</p>
@@ -15,16 +39,42 @@ class SuperAdmin extends PureComponent {
           </button>
         </NavLink>
 
-        <NavLink to="/superadmin/table">
-        <button type="button" className="btn btn-info">
-            View Table
-          </button>
-        </NavLink>
-
-        <div className="super-form">
-          <Route path="/superadmin/superform" component={SuperForm} />
-          <Route path="/superadmin/table" component={SuperTable} />
-        </div>
+        <div className="tableData">
+        <h1 id="title">Table Data</h1>
+        <table id="usersdata">
+          <thead>
+            <tr>
+              <th>Email</th>
+              <th>Full Name</th>
+              <th>Role</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tableUser.length > 0 ? (
+              tableUser.map((user, index) => {
+                return (
+                  <tr key={index}>
+                    <td>{user.email}</td>
+                    <td>
+                      {user.first_name} {user.last_name}
+                    </td>
+                <td>{user.role}</td>
+                    <td className="btn-container">
+                      <button className="edit">edit</button>
+                      <button className="delete">delete</button>
+                    </td>
+                  </tr>
+                );
+              })
+            ) : (
+              <tr>
+                <td colSpan="5">Loading...</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
+      </div>
       </div>
     );
   }
