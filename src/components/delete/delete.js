7e625da11d6b650338f05069702b51881edcba46
tcap_Abra <Guerrero-rollon.A1@trans-cosmos.co.jp>
2020-09-01 10:14:20 +0800

    fix

--- src/components/delete/delete.js
+++ src/components/delete/delete.js
@@ -1,41 +1,34 @@
-import React, {PureComponent} from 'react';
-import { getUsers, deleteUser } from "../../graphqlAPI";
-
+import React, { PureComponent } from "react";
+import { getUsers } from "../../graphqlAPI";
 
 class Delete extends PureComponent {
-    constructor(props){
-        super(props);
-        this.state = {
-            tableUser:[{
-                email:'',
-                first_name:'',
-                last_name: '',
-                role:''
-            }],
-        }
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      tableUser: [
+        {
+          email: "",
+          first_name: "",
+          last_name: "",
+          role: "",
+        },
+      ],
+    };
+  }
 
-   async componentDidMount() {
-        const {users} = this.props;
-        await getUsers(users)
-         .then (data => {
-             this.setState({tableUser: data})
-             console.log(data)
-         })
+  async componentDidMount() {
+    const { users } = this.props;
+    await getUsers(users).then((data) => {
+      this.setState({ tableUser: data });
+      console.log(data);
+    });
+  }
 
-    }
+  removeData() {}
 
-    removeData() {
-        
-    }
-
-    render() {
-       
-        return(
-        <p>test</p>
-        )
-    }
-
+  render() {
+    return <p>test</p>;
+  }
 }
 
 export default Delete;
